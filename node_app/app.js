a739e8a741ce7363f2e18c24728d1e2c87b460b1
Joseph Huttner <joseph.huttner@gmail.com>
2012-04-25 04:57:45 +0000

    More preliminary work.

--- node_app/app.js
+++ node_app/app.js
@@ -1,12 +1,38 @@
+var fs = require('fs');
 
 /**
  * Module dependencies.
  */
 
 var express = require('express')
+  , client = require('redis').createClient()
   , routes = require('./routes')
 
 var app = module.exports = express.createServer();
+
+/**
+ * Module setup.
+ */
+
+app.use(express.bodyParser());
+
+/**
+ * Utility functions
+ */
+
+var read_config = function() {
+  var data = fs.readFileSync('config.json');
+  return JSON.loads(data);
+};
+
+var get_random_string = function(len) {
+  var s = "abcdefghijkmnopqrstuvwxyz";
+  var result = '';
+  while (result.length < len) {
+    result += s[Math.floor(Math.random() * s.length)]
+  }
+  return result;
+};
 
 // Configuration
 
@@ -31,5 +57,54 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.get('/pastie/user/:user', function(req, res){
+  client.
+  res.send(JSON.stringify({pasties: result});
+  //res.send(JSON.stringify({
+    //pasties: [
+      //{
+        //id: "goot",
+        //description: "this is a brief description",
+        //content: "This is is the hypothetical body",
+      //},
+      //{
+        //id: "xhlx",
+        //description: "a bit different description",
+        //content: "A different body",
+      //},
+    //]
+  //}));
+});
+
+app.get('/pastie/id/:id', function(req, res) {
+  client.hget
+  res.send(JSON.stringify({pastie: result});
+});
+
+app.post('/pastie', function(req, res) {
+  var id;
+
+  while (1) {
+    id = get_random_string(4)
+    if client.exists("pastie:" + id, function(err, result) {
+      if (!result) { break; }
+    });
+  }
+
+  client.hmset("pastie:" + id, req.body, function(err, result) {
+    if (err) { return cb(err); }
+    client.lpush("user_pasties:" + req.body.user, id, function(err, result) {
+      //if (err) { return cb(err); }
+      if (!req.body.private) {
+        client.lpush("public_pasties:", id);
+      }
+      req.body.id = id;
+      res.send(JSON.dumps(req.body));
+    });
+  });
+});
+
+// TODO: pretty UI items
+
+app.listen(4000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ pastie.py
@@ -0,0 +1,73 @@
+#!/usr/bin/pythan
+
+from optparse import OptionParser
+import sys
+import getpass
+
+parser = OptionParser( description="Pastie is a secure internal paste bin.")
+
+#parser.add_option("-p", dest="private",
+    #action="store_true", help="create private pastie")
+parser.add_option("-u", dest="user",
+    action="store", help="list pasties created by USER")
+parser.add_option("-s", dest="ID",
+    action="store", help="print the pastie to stdout")
+#parser.add_option("-x", dest="expression",
+    #action="store", help="delete the pastie in the future. E.g. 10m, 1d, 2h")
+parser.add_option("-d", dest="ID",
+    action="store", help="delete the pastie")
+
+(options, args) = parser.parse_args()
+
+
+def read_config():
+  handle = open("config.json")
+  if not handle:
+    return False
+  return handle.read()
+
+def save_pastied(options, config):
+  payload = simplejson.dumps({
+    description: options.description or False,
+    #private: options.store_true or False,
+    content: sys.stdin.readlines(),
+    author: getpass.getuser() or False,
+    #expiry: convert_expiry_to_seconds(options.expiry) if options.expiry else False,
+  })
+  jdata = json.dumps({"username":"...", "password":"..."})
+  res = urllib2.urlopen(config.server + "/pastie", payload).read()
+  return res
+
+def convert_expiry_to_seconds(expiry_expr):
+  scalar = {
+    "s": 1,
+    "m": 60,
+    "h": 60*60,
+    "d": 60*60*24,
+    "w": 60*60*24*7,
+    "m": 60*60*24*7*30,
+    "y": 60*60*24*7*365,
+  }[expiry_expr[-1]]
+  num = int(expiry_expr[:-1])
+  return False if not num else num*scalar
+
+def main():
+  config = read_config()
+  if not config:
+    print "You must include a config file."
+  elif "user" in options:
+    res = urllib2.urlopen(config.server + "/pastie/user/" + options.user).read()
+    res = JSON.loads(res)
+    for r in res.pasties:
+      print r.resource, "    ", r.description 
+  elif "pastie_id" in options:
+    res = urllib2.urlopen(config.server + "/pastie/id/" + options.pastie_id).read()
+    res = JSON.loads(res)
+    print res.pastie.content
+  else:
+    res = save_pastied(options, config)
+    print res.pastie.resource
+  print
+
+if __name__ == "__main__":
+  main()
