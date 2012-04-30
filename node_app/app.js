1dd5f7c46867796f1091dc002689d8d3133329b3
Joseph Huttner <joseph.huttner@gmail.com>
2012-04-30 00:28:20 +0000

    Tabs to spaces.

--- node_app/app.js
+++ node_app/app.js
@@ -40,110 +40,110 @@ var get_random_string = function(len) {
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-	app.use(express.bodyParser());
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/pastie/user/:user', function(req, res){
-	res.send("Not yet implemented.");
+  res.send("Not yet implemented.");
 });
 
 app.get('/', function(req, res) {
-	config = read_config()
-	client.zrangebyscore("leaderboard", "-inf", "+inf", function(err, users) {
-		client.lrange("public_pasties", 0, 20, function(err, public_pasties) {
-			if (err) { console.log(err); }
-			var remaining = public_pasties.length;
-			console.log("remaining start", remaining);
-			if (!public_pasties.length) {
-				res.render("index.jade", {
-					title: "Pastie",
-					host: config.host,
-					leaderboard: users,
-					public_pasties: public_pasties,
-				});
-			} else {
-				_.each(public_pasties, function(id, index) {
-					client.hgetall("pastie:" + id, function(err, result) {
-						public_pasties.push(result);
-						remaining -= 1;
-						console.log("remaining now", remaining);
-						if (!remaining) {
-							res.render("index.jade", {
-								title: "Pastie",
-								host: config.host,
-								leaderboard: users,
-								public_pasties: public_pasties,
-							});
-						}
-					});
-				});
-			}
-		});
-	});
+  config = read_config()
+  client.zrangebyscore("leaderboard", "-inf", "+inf", function(err, users) {
+    client.lrange("public_pasties", 0, 20, function(err, public_pasties) {
+      if (err) { console.log(err); }
+      var remaining = public_pasties.length;
+      console.log("remaining start", remaining);
+      if (!public_pasties.length) {
+        res.render("index.jade", {
+          title: "Pastie",
+          host: config.host,
+          leaderboard: users,
+          public_pasties: public_pasties,
+        });
+      } else {
+        _.each(public_pasties, function(id, index) {
+          client.hgetall("pastie:" + id, function(err, result) {
+            public_pasties.push(result);
+            remaining -= 1;
+            console.log("remaining now", remaining);
+            if (!remaining) {
+              res.render("index.jade", {
+                title: "Pastie",
+                host: config.host,
+                leaderboard: users,
+                public_pasties: public_pasties,
+              });
+            }
+          });
+        });
+      }
+    });
+  });
 });
 
 app.get('/pastie/:id', function(req, res) {
-	client.hgetall("pastie:" + req.params.id, function(err, result) {
-		if (err) { console.log(err); }
-		res.setHeader("content-type", "text/plain");
-		res.send(result.content);
-	});
+  client.hgetall("pastie:" + req.params.id, function(err, result) {
+    if (err) { console.log(err); }
+    res.setHeader("content-type", "text/plain");
+    res.send(result.content);
+  });
 });
 
 app.post('/pastie', function(req, res) {
-	console.log(req.body);
+  console.log(req.body);
   var fn = function() {
-		var id = get_random_string(5);
+    var id = get_random_string(5);
     client.exists("pastie:" + id, function(err, exists) {
-			if (exists) {
-				console.log("exists", id);
-				return fn(); 
-			} else {
-				var pastie = req.body.pastie;
+      if (exists) {
+        console.log("exists", id);
+        return fn();
+      } else {
+        var pastie = req.body.pastie;
 
-				if (!pastie) {
-					return res.send(JSON.stringify({"error": "invalid JSON passed"}));
-				}
+        if (!pastie) {
+          return res.send(JSON.stringify({"error": "invalid JSON passed"}));
+        }
 
-				pastie.id = id;
-				pastie.created = (new Date()).getTime();
+        pastie.id = id;
+        pastie.created = (new Date()).getTime();
 
-				client.hmset("pastie:" + id, pastie, function(err, result) {
-					if (err) { 
-						return cb(err); 
-					}
-					client.lpush("user_pasties:" + pastie.user, id, function(err, result) {
-						if (err) { 
-							return cb(err); 
-						}
-						if (!pastie.private) {
-							client.lpush("public_pasties", id);
-						}
-						if (pastie.expiry) {
-							client.expire("pastie:" + id, pastie.expiry);
-						}
-						client.zincrby("leaderboard", 1, pastie.author);
-						res.send(JSON.stringify({pastie: {id: id}}));
-					});
-				});
-			}
+        client.hmset("pastie:" + id, pastie, function(err, result) {
+          if (err) {
+            return cb(err);
+          }
+          client.lpush("user_pasties:" + pastie.user, id, function(err, result) {
+            if (err) {
+              return cb(err);
+            }
+            if (!pastie.private) {
+              client.lpush("public_pasties", id);
+            }
+            if (pastie.expiry) {
+              client.expire("pastie:" + id, pastie.expiry);
+            }
+            client.zincrby("leaderboard", 1, pastie.author);
+            res.send(JSON.stringify({pastie: {id: id}}));
+          });
+        });
+      }
     });
   };
 
-	fn();
+  fn();
 });
 
 app.listen(4000);
