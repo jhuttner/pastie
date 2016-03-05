2d1795c1a70df794228e6758628c44a9382ca572
Joseph Huttner <jhuttner@appnexus.com>
2016-03-05 00:27:24 +0000

    Remove unused methods and overall cleanup

--- node_app/app.js
+++ node_app/app.js
@@ -1,42 +1,39 @@
+// Native imports
+
 var fs = require('fs');
+
+
+// Third-party imports
+
 var _ = require('underscore');
+var express = require('express');
+var client = require('redis').createClient();
 
-/**
- * Module dependencies.
- */
 
-var express = require('express')
-  , client = require('redis').createClient()
-  , routes = require('./routes')
+// Application setup
 
 var app = module.exports = express.createServer();
-
-/**
- * Module setup.
- */
-
 app.use(express.bodyParser());
 
-/**
- * Utility functions
- */
+
+// Utility functions
 
 var read_config = function() {
-  var data = fs.readFileSync('config.json');
+  var data = fs.readFileSync('/etc/pastie/config.json');
   return JSON.parse(data);
 };
 
 var get_random_string = function(len) {
-  var s = "abcdefghijkmnopqrstuvwxyz";
+  var s = 'abcdefghijkmnopqrstuvwxyz';
   var result = '';
   while (result.length < len) {
-    result += s[Math.floor(Math.random() * s.length)]
+    result += s[Math.floor(Math.random() * s.length)];
   }
   return result;
 };
 
 var get_extension  = function(content) {
-  if (Boolean(~content.indexOf("<html>"))) {
+  if (Boolean(~content.indexOf('<html>'))) {
     return '.html';
   } else {
     var result = get_image_type(content);
@@ -45,16 +42,16 @@ var get_extension  = function(content) {
     }
   }
   return '';
-}
+};
 
 var get_image_type = function(content) {
-  if (!content.indexOf("\xFF\xD8\xFF")) {
-    return '.jpeg'
-  } else if (!content.indexOf("GIF")){
-    return '.gif'
-  } else if (!content.indexOf("\x89\x50\x4e\x47\x0d\x0a")) {
-    return '.png'
-  } else if (!content.indexOf("BM")) {
+  if (!content.indexOf('\xFF\xD8\xFF')) {
+    return '.jpeg';
+  } else if (!content.indexOf('GIF')){
+    return '.gif';
+  } else if (!content.indexOf('\x89\x50\x4e\x47\x0d\x0a')) {
+    return '.png';
+  } else if (!content.indexOf('BM')) {
     return '.bmp';
   } else if (!content.indexOf('8BPS')) {
     return '.psd';
@@ -63,6 +60,7 @@ var get_image_type = function(content) {
   }
   return null;
 };
+
 
 // Configuration
 
@@ -83,49 +81,64 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
 
-app.get('/pastie/user/:user', function(req, res){
-  res.send("Not yet implemented.");
-});
+// Routes
 
 app.get('/', function(req, res) {
   var config = read_config();
-  client.zrangebyscore("leaderboard", "-inf", "+inf", function(err, users) {
-    client.lrange("public_pasties", 0, 20, function(err, public_pasties) {
-      if (err) {
-        return res.send(err);
-      }
-      var remaining = public_pasties.length;
-      if (!public_pasties.length) {
-        res.render("index.jade", {
-          title: "Pastie",
-          host: config.host + ":" + config.port,
-          leaderboard: users,
-          public_pasties: public_pasties,
+  var public_pasties = [];
+  client.lrange('public_pastie_ids', 0, 100, function(err, public_pastie_ids) {
+    if (err) {
+      // No public pasties yet.  Continue as normal.
+    }
+    if (public_pastie_ids.length) {
+      _.each(public_pastie_ids, function(id, index) {
+        client.hgetall('pastie:' + id, function(err, result) {
+          public_pasties.push(result);
         });
-      } else {
-        _.each(public_pasties, function(id, index) {
-          client.hgetall("pastie:" + id, function(err, result) {
-            public_pasties.push(result);
-            remaining -= 1;
-            if (!remaining) {
-              res.render("index.jade", {
-                title: "Pastie",
-                host: config.host + ":" + config.port,
-                leaderboard: users,
-                public_pasties: public_pasties,
-              });
-            }
-          });
-        });
-      }
+      });
+    }
+    client.hgetall('pastie_users', function(err, pastie_users) {
+      pastie_users_sorted = Object.keys(pastie_users).sort();
+      res.render('index.jade', {
+        title: 'Pastie',
+        host: config.host + ':' + config.port,
+        public_pasties: public_pasties,
+        pastie_users: pastie_users,
+        pastie_users_sorted: pastie_users_sorted,
+      });
     });
   });
 });
 
+app.get('/pastie/:id', function(req, res) {
+  client.hgetall('pastie:' + req.params.id, function(err, result) {
+    if (err) {
+      return res.send(err);
+    }
+    else if (result === null || Object.keys(result).length === 0) {
+      res.setHeader('Content-Type', 'text/plain');
+      return res.send('No pastie exists with the provided ID. Perhaps the pastie expired?');
+    } else {
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(result.content);
+    }
+  });
+});
+
+app.delete('/pastie/:id', function(req, res) {
+  client.del(['pastie:' + req.params.id], function(err, result) {
+    res.setHeader('Content-Type', 'application/json');
+    if (result === 0) {
+      res.send(JSON.stringify({'message': 'Error: No Pastie found with ID'}));
+    } else {
+      res.send(JSON.stringify({'message': 'Pastie deleted successfully'}));
+    }
+  });
+});
+
 app.get('/pastie/:id.html', function(req, res) {
-  client.hgetall("pastie:" + req.params.id, function(err, result) {
+  client.hgetall('pastie:' + req.params.id, function(err, result) {
     if (err) {
       return res.send(err);
     }
@@ -134,91 +147,56 @@ app.get('/pastie/:id.html', function(req, res) {
 });
 
 app.get('/pastie/:id.:extension(jpeg|gif|png|bmp|psd|swf)', function(req, res) {
-  client.hgetall("pastie:" + req.params.id, function(err, result) {
+  client.hgetall('pastie:' + req.params.id, function(err, result) {
     if (err) {
       return res.send(err);
     }
-    res.setHeader("Content-Type", "image/" + req.params.extension);
+    res.setHeader('Content-Type', 'image/' + req.params.extension);
     res.write(result.content, 'binary');
     res.end();
-  });
-});
-
-app.get('/pastie/:id', function(req, res) {
-  client.hgetall("pastie:" + req.params.id, function(err, result) {
-    if (err) {
-      return res.send(err);
-    }
-    res.setHeader("Content-Type", "text/plain");
-    res.send(result.content);
-  });
-});
-
-app.get('/pastie/topic/:topic', function(req, res) {
-  client.lrange("pastie_topic:" + req.params.topic, 0, -1, function(err, result) {
-    if (err) {
-      return res.send(err);
-    }
-
-    var batch_cmds = client.multi();
-    _.each(result, function(pastie_id) {
-        batch_cmds.hgetall("pastie:" + pastie_id);
-    });
-    // Execute the batch command and send the text content
-    batch_cmds.exec(function(error, responses) {
-        if (error) {
-          return res.send(error);
-        }
-        var output = "";
-        _.each(responses, function(r) {
-            var str = r.author + ": " + r.content;
-            output = output + str;
-        });
-        res.setHeader("content-type", "text/plain");
-        res.send(output);
-    });
   });
 });
 
 app.post('/pastie', function(req, res) {
   var fn = function() {
     var id = get_random_string(5);
-    client.exists("pastie:" + id, function(err, exists) {
+    client.exists('pastie:' + id, function(err, exists) {
       if (exists) {
         return fn();
       } else {
         var pastie = req.body.pastie;
         pastie.content = new Buffer(pastie.content, 'base64').toString('binary');
-        var is_html = Boolean(~pastie.content.indexOf("<html>"));
+        var is_html = Boolean(~pastie.content.indexOf('<html>'));
         var is_img = get_image_type(pastie.content);
 
         if (!pastie) {
-          return res.send(JSON.stringify({"error": "invalid JSON passed"}));
+          return res.send(JSON.stringify({'error': 'invalid JSON passed'}));
         }
 
         pastie.id = id;
         pastie.created = (new Date()).getTime();
         pastie.extension = get_extension(pastie.content);
 
-        client.hmset("pastie:" + id, pastie, function(err, result) {
+        client.hincrby('pastie_users', pastie.author, 1, function(err, result) {
           if (err) {
-            return res.send(err);
+            res.send(err);
           }
-          client.lpush("user_pasties:" + pastie.author, id, function(err, result) {
+          client.hmset('pastie:' + id, pastie, function(err, result) {
             if (err) {
-              return cb(err);
+              return res.send(err);
             }
-            if (!pastie.private) {
-              client.lpush("public_pasties", id);
-            }
-            if (pastie.expiry) {
-              client.expire("pastie:" + id, pastie.expiry);
-            }
-            if (pastie.topic) {
-              client.lpush("pastie_topic:" + pastie.topic, id);
-            }
-            client.zincrby("leaderboard", 1, pastie.author);
-            res.send(JSON.stringify({pastie: {id: id, extension: pastie.extension}}));
+            client.lpush('user_pasties:' + pastie.author, id, function(err, result) {
+              if (err) {
+                return cb(err);
+              }
+              if (pastie.public) {
+                client.lpush('public_pastie_ids', id);
+              }
+              if (pastie.expiry) {
+                client.expire('pastie:' + id, pastie.expiry);
+              }
+              res.send(JSON.stringify({pastie: {id: id, extension: pastie.extension}}));
+            });
           });
         });
       }
@@ -228,5 +206,19 @@ app.post('/pastie', function(req, res) {
   fn();
 });
 
-app.listen(read_config().port);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Main
+(function() {
+  var port;
+  if (read_config().port) {
+    port = read_config().port;
+  } else {
+    if (read_config().host.indexOf('https') > -1) {
+      port = 443;
+    } else {
+      port = 80;
+    }
+  }
+  app.listen(port);
+  console.log('Express server listening on port %d in %s mode', app.address().port, app.settings.env);
+}());
--- pastie.py
+++ pastie.py
@@ -1,46 +1,74 @@
 #!/usr/bin/python
 
-from optparse import OptionParser
-import os
-import sys
-import fileinput
-import getpass
-import urllib2
 import base64
+import getpass
+from optparse import OptionParser
+import sys
+import urllib2
+
+
+CONFIG_FILE_PATH = '/etc/pastie/config.json'
+CONFIG_FILE_EXAMPLE = '''
+{
+   "host": "https://pastie.yoursite.com",
+   "port": "3000" # This field is optional
+}
+'''
 
 try:
   import json
 except:
   import simplejson as json
 
-parser = OptionParser(description="Pastie is a secure internal paste bin.")
 
-parser.add_option("-d", "--description", dest="description",
-    action="store", help="description")
-parser.add_option("-p", "--private", dest="private",
-    action="store_true", help="create private pastie")
-parser.add_option("-t", "--topic", dest="topic",
-    action="store", help="append to a pastie topic thread")
-parser.add_option("--print", dest="print_id",
-    action="store", help="print the pastie to stdout")
-parser.add_option("--print-topic", dest="print_topic",
-    action="store", help="print the pastie topic thread to stdout")
-parser.add_option("-x", "--expires", dest="expiry",
-    action="store", help="delete the pastie in the future. E.g. 10m, 1d, 2h")
-#parser.add_option("-l", dest="user",
-    #action="store", help="list pasties created by USER")
-#parser.add_option("-d", dest="delete_id",
-    #action="store", help="delete the pastie")
+parser = OptionParser(description='Pastie is a secure internal paste bin.')
+parser.add_option('-d', '--delete', dest='delete',
+    help='delete an existing pastie')
+parser.add_option('-p', '--public', dest='public',
+    action='store_true', help='create a public pastie (listed on pastie homepage)')
+parser.add_option('-x', '--expires', dest='expiry',
+    action='store', help='Expire the pastie after expiry units. E.g. 1s, 2m, 3s, 4m, 5d, 5w, 6m, 7y.  No units implies days.')
 
-(options, args) = parser.parse_args()
 
+def convert_expiry_to_seconds(text):
+  try:
+    return int(text) * 60*60*24
+  except:
+    pass
+  scalar = {
+    's': 1,
+    'm': 60,
+    'h': 60*60,
+    'd': 60*60*24,
+    'w': 60*60*24*7,
+    'm': 60*60*24*7*30,
+    'y': 60*60*24*7*365,
+  }[text[-1]]
+  num = int(text[:-1])
+  if not num:
+    return False
+  else:
+    return num*scalar
 
 def read_config():
-  #print os.path.dirname(os.path.realpath(__file__))
-  handle = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json"))
-  if not handle:
-    return None
-  return json.loads(handle.read())
+  f = open(CONFIG_FILE_PATH)
+  result = json.load(f)
+  f.close()
+  return result
+
+def get_url(config):
+  if config.get('port'):
+    url = '%s:%s' % (config['host'], config['port'])
+  else:
+    url = '%s' % (config['host'])
+  return url
+
+def delete_pastie(options, config):
+  req = urllib2.Request(get_url(config) + '/pastie/' + options.delete)
+  req.get_method = lambda: 'DELETE'
+  res = urllib2.urlopen(req).read()
+  print "RES IS", res
+  return json.loads(res)
 
 def save_pastie(options, config):
   data = []
@@ -51,76 +79,46 @@ def save_pastie(options, config):
       break
     data.append(s)
 
-  payload = {"content": base64.b64encode(''.join(data)), "author": getpass.getuser()}
+  payload = {
+    'content': base64.b64encode(''.join(data)), 
+    'author': getpass.getuser()
+  }
 
-  if options.description:
-    payload["description"] = options.description;
-
-  if options.private:
-    payload["private"] = 1;
+  if options.public:
+    payload['public'] = 1;
 
   if options.expiry:
-    payload["expiry"] = convert_expiry_to_seconds(options.expiry)
+    payload['expiry'] = convert_expiry_to_seconds(options.expiry)
 
-  if options.topic:
-    payload["topic"] = options.topic
+  payload = json.dumps({'pastie': payload});
 
-  payload = json.dumps({"pastie": payload});
-
-  #print payload
-  url = "%s:%d/pastie" % (config["host"], config["port"])
-  req = urllib2.Request(url, payload, {"content-type": "application/json"})
+  req = urllib2.Request(get_url(config) + '/pastie', payload, {'content-type': 'application/json'})
   res = urllib2.urlopen(req).read()
   return json.loads(res)
 
-def convert_expiry_to_seconds(expiry_expr):
-  scalar = {
-    "s": 1,
-    "m": 60,
-    "h": 60*60,
-    "d": 60*60*24,
-    "w": 60*60*24*7,
-    "m": 60*60*24*7*30,
-    "y": 60*60*24*7*365,
-  }[expiry_expr[-1]]
-  num = int(expiry_expr[:-1])
-  if not num:
-    return False
-  else:
-    return num*scalar
-
 def main():
-  config = read_config()
-  if not config:
-    print "You must include a config file."
-  #elif options.user:
-    #res = urllib2.urlopen(config["host"] + "/pastie/user/" + options.user).read()
-    #res = JSON.loads(res)
-    #for r in res["pasties"]:
-      #print r["resource"], "    ", r["description"]
-  #elif options.pastie_id:
-    #res = urllib2.urlopen(config["host"] + "/pastie/" + options.pastie_id).read()
-    #res = JSON.loads(res)
-    #print res
-    #print res["pastie"]["content"]
-  elif options.print_id:
-    url = "%s:%d/pastie/%s" % (config["host"], config["port"], options.print_id)
-    res = urllib2.urlopen(url).read()
-    print res
-  elif options.print_topic:
-    url = "%s:%d/pastie/topic/%s" % (config["host"], config["port"], options.print_topic)
-    res = urllib2.urlopen(url).read()
-    print res
+
+  (options, args) = parser.parse_args()
+  try:
+    config = read_config()
+  except IOError:
+    print 'You must include a config file at ', CONFIG_FILE_PATH
+    print 'Example:'
+    print CONFIG_FILE_EXAMPLE
+    sys.exit(1)
+
+  if options.delete:
+    res = delete_pastie(options, config)
+    print res['message']
   else:
     res = save_pastie(options, config)
-    if res["pastie"]["id"]:
-      url = "%s:%d/pastie/%s%s" % (config["host"],
-                                   config["port"],
-                                   res["pastie"]["id"],
-                                   res["pastie"]["extension"])
+    if res['pastie']['id']:
+      url = '%s/pastie/%s%s' % (get_url(config),
+                                res['pastie']['id'],
+                                res['pastie']['extension'])
       print url
-    elif res["error"]:
-      print res["error"]
+    elif res['error']:
+      print res['error']
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
